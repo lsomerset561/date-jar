edb1f3ae8da25f7774da9ad4383fd1450c306c0c
Lindsey Somerset <somerset.lindsey@gmail.com>
2016-04-15 04:09:04 -0400

    remove text decoration on start button

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <div class="col-xs-12 col-md-7 spacer">
 	<h4 class="text-center spacer">Enter at least two date ideas &amp; I'll randomly choose one for you!</h4>
-	<a href="start.php">
+	<a href="start.php" id="start-button">
 		<button class="btn btn-default btn-lg center-block" type="submit" name="start" value="start">
 			Start
 		</button>
